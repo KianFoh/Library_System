87a77c34f21809dfa3834fea5d5b7068967fbe0f
ThienKianFoh <thienkianfoh@gmail.com>
2024-03-25 14:16:27 +0800

    Email activation message

--- library_system_backend/authentication/views.py
+++ library_system_backend/authentication/views.py
@@ -1,15 +1,25 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.models import User
+from django.contrib import messages
 from .forms import SignupForm
+from django.utils.safestring import mark_safe
+
+
+def activateEmail(request, user, to_email):
+    message_content = mark_safe(f'Dear <b>{user}</b>, please go to your email <b>{to_email}</b> inbox and click on \
+                     the activation link to complete the registration.')
+    messages.success(request, message_content)
 
 def signup_view(request):
     if request.method == 'POST':
         form = SignupForm(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
-            user.is_active = False  # User is not active until email is confirmed
+            user.is_active = False
             user.save()
+            activateEmail(request, user, form.cleaned_data.get('email'))
+            return redirect('/')
     else:
         form = SignupForm()
     return render(request, 'authentication/signup.html', {'form': form})
