83278fdd40d3305b5a557faaae16fd8ff9fecc10
ThienKianFoh <thienkianfoh@gmail.com>
2024-03-25 16:51:17 +0800

    Email verification signup completed

--- library_system_backend/authentication/views.py
+++ library_system_backend/authentication/views.py
@@ -1,18 +1,29 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login
-from django.contrib.auth.models import User
+from django.contrib.auth import login, logout, authenticate, get_user_model
 from django.contrib import messages
 from .forms import SignupForm
 from django.utils.safestring import mark_safe
 from django.template.loader import render_to_string
 from django.contrib.sites.shortcuts import get_current_site
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
-from django.utils.encoding import force_bytes, force_bytes
+from django.utils.encoding import force_bytes, force_str
 from django.core.mail import EmailMessage
 from .token import account_activation_token
 
 
-def activate(request,uidb64,token):
+def activate(request,uidb64, token):
+    User = get_user_model()
+    try:
+        uid = force_str(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except:
+        user = None
+
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+
+        messages.success(request, "Thank you for your email confirmation. Now you can login your account.")
     return redirect('/')
 
 def activateEmail(request, user, to_email):
