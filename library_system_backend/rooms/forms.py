8d4e970c6ac802f5638f57762de05e0dce14244b
ThienKianFoh <thienkianfoh@gmail.com>
2024-05-27 16:39:43 +0800

    Room backend 100% completed 1) Booking model has some update require makemigrations and migrate

--- library_system_backend/rooms/forms.py
+++ library_system_backend/rooms/forms.py
@@ -1,14 +1,42 @@
 from django import forms
 
-
 class RoomForm(forms.Form):
-    timeslots = forms.MultipleChoiceField(choices=[],
-                                          widget=forms.CheckboxSelectMultiple(attrs={'class': 'timeslot-checkbox'}),
-                                          label='')
-    usernames = forms.CharField(
-        label='User Names',
-        max_length=100,
-        help_text='Enter multiple usernames separated by spaces',
-        required=True
+    timeslots = forms.MultipleChoiceField(
+        choices=[], 
+        widget=forms.CheckboxSelectMultiple(attrs={'class': 'timeslot-checkbox'}), 
+        label='',
+        required=False
+    )
+    number_of_users = forms.ChoiceField(
+        label='Number of Users'
     )
 
+    def __init__(self, *args, **kwargs):
+        min_users = kwargs.pop('min_users', 1)
+        max_users = kwargs.pop('max_users', 10)
+        super(RoomForm, self).__init__(*args, **kwargs)
+        self.add_username_fields()
+        self.fields['number_of_users'].choices = [(i, str(i)) for i in range(min_users, max_users + 1)]
+
+
+    def add_username_fields(self):
+        # Determine the number of users based on form data or initial value
+        if self.is_bound:  # Form is bound to POST data
+            number_of_users = int(self.data.get('number_of_users', 1))
+        else:  # Initial load
+            number_of_users = int(self.initial.get('number_of_users', 1))
+
+        # Add username fields based on the number of users
+        for i in range(1, number_of_users + 1):
+            self.fields[f'username_{i}'] = forms.CharField(
+                label=f'Username {i}',
+                max_length=100,
+                required=True
+            )
+
+    def clean(self):
+        cleaned_data = super().clean()
+        number_of_users = int(cleaned_data.get('number_of_users', 1))
+        usernames = [cleaned_data.get(f'username_{i}') for i in range(1, number_of_users + 1)]
+        cleaned_data['usernames'] = usernames
+        return cleaned_data
--- library_system_backend/rooms/views.py
+++ library_system_backend/rooms/views.py
@@ -1,10 +1,20 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.template import loader
 from django.http import HttpResponse
-from .models import Room 
-from bookings.models import Timeslot 
+from .models import Room
+from bookings.models import Timeslot, Booking, BookingUser
 from datetime import datetime
 from .forms import RoomForm
+from django.contrib import messages
+from django.utils.safestring import mark_safe
+from django.contrib.auth.models import User
+from user_data.models import User_data 
+from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
+from django.core.mail import EmailMessage
 
 def rooms(request):
     rooms_data = Room.objects.order_by('id')
@@ -22,6 +32,51 @@ def rooms(request):
 
     return HttpResponse(template.render(context, request))
 
+# Function to send booking confirmation email
+def send_booking_email(users, initiator, room, room_timeslots, request):
+    recipients = [user.email for user in users]
+
+    # Construct the URL for the website
+    website_url = "http://127.0.0.1:8000/"
+
+    subject = 'Booking Confirmation'
+    message = (
+        f'Booking Confirmation\n\n'
+        f'Hello,\n\n'
+        f'You have a pending booking confirmation.\n\n'
+        f'Room: {room.name}\n\n'
+        f'Timeslot:\n'
+    )
+
+    for timeslot in room_timeslots:
+        message += f'{timeslot.start_time.strftime("%I:%M %p")} - {timeslot.end_time.strftime("%I:%M %p")}\n'
+
+    # Construct the students' names string
+    students_names = ", ".join(user.username for user in users)
+    
+    # Add (Initiator) after the initiator's name if it's a student
+    students_names_list = students_names.split(", ")
+    if initiator.username in students_names_list:
+        students_names_list[students_names_list.index(initiator.username)] += " (Initiator)"
+        students_names = ", ".join(students_names_list)
+
+    message += (
+        f'\nStudents: {students_names}.\n'
+        f'Please go to the booking page on the library room booking website to confirm the booking:\n'
+        f'{website_url}\n\n'
+        'If you don\'t approve, please disregard this email.\n\n'  # Adjusted to "don't approve"
+        'Thank you.\n'
+    )
+
+    try:
+        email = EmailMessage(subject, message, to=recipients)
+        email.send()
+        message_content = 'Booking has been added. Please go to the booking page to confirm.'
+        messages.success(request, message_content)
+    except Exception as e:
+        message_content = f'Failed to send booking confirmation email to {", ".join(recipients)}: {str(e)}'
+        messages.warning(request, message_content)
+
 def room(request, room_id):
     room_data = get_object_or_404(Room, pk=room_id)
     room_timeslots = Timeslot.objects.filter(room_id=room_id)
@@ -32,8 +87,72 @@ def room(request, room_id):
         timeslot_display = f"{timeslot.start_time.strftime('%I:%M %p')} - {timeslot.end_time.strftime('%I:%M %p')}"
         if timeslot.status == 'Empty':
             timeslot_choices.append((timeslot.id, timeslot_display))
+    
+    if timeslot_choices:
+        if request.method == 'POST':
+            form = RoomForm(request.POST, min_users=room_data.min_pax, max_users=room_data.max_pax)
+            form.fields['timeslots'].choices = timeslot_choices
+            if form.is_valid():
+                # Process form data
+                usernames = form.cleaned_data['usernames']
+                selected_timeslots_id = form.cleaned_data['timeslots']
+                num_timeslots = len(selected_timeslots_id)
+                
+                if not request.user.is_authenticated:
+                    message_content = mark_safe('Please log in to your account before adding a booking.')
+                    messages.warning(request, message_content)                    
+                elif not num_timeslots:
+                    message_content = mark_safe('Please select a timeslot from the available options.')
+                    messages.warning(request, message_content)
+                elif request.user.username not in usernames:
+                    message_content = mark_safe('Your username must be included in the usernames.')
+                    messages.warning(request, message_content)
+                else:
+                    users_data = []
+                    time_slots_data = []
 
-    form = RoomForm()
-    form.fields['timeslots'].choices = timeslot_choices
+                    for username in usernames:
+                        try:
+                            user_temp = User.objects.get(username=username)
+                            user_addit_data = User_data.objects.get(user=user_temp)
+                            users_data.append(user_temp)          
+                            
+                            if user_addit_data.room_usage_hour == 2:
+                                message_content = mark_safe(f'{username} has reached the maximum daily booking limit of 2 hours.')
+                                messages.warning(request, message_content)
+                                return redirect('room', room_id=room_id)  
+                            
+                            elif user_addit_data.room_usage_hour + num_timeslots > 2:
+                                message_content = mark_safe(f'{username} does not have enough remaining booking hours. Maximum daily booking limit is 2 hours. {username} has {2-user_addit_data.room_usage_hour} hours left.')
+                                messages.warning(request, message_content)
+                                return redirect('room', room_id=room_id)  
+                                
+                        except User.DoesNotExist:   
+                            message_content = mark_safe(f'{username} is not a valid username.')
+                            messages.warning(request, message_content)
+                            return redirect('room', room_id=room_id)     
+
+                    for selected_timeslot_id in selected_timeslots_id:
+                        time_slot_data = Timeslot.objects.get(id=selected_timeslot_id)
+                        time_slots_data.append(time_slot_data)
+
+                    new_booking = Booking.objects.create()
+                    new_booking.time_slot.set(time_slots_data)
+
+                    # Create BookingUser instances for each user in users_data
+                    for user in users_data:
+                        booking_user = BookingUser.objects.create(booking=new_booking, user=user)
+                        booking_user.save()
+
+                    new_booking.save()
+
+                    #Send booking confirmation email to all users involve
+                    send_booking_email(users_data, request.user, room_data, time_slots_data, request)
+
+        else:
+            form = RoomForm(min_users=room_data.min_pax, max_users=room_data.max_pax)
+            form.fields['timeslots'].choices = timeslot_choices
+    else:
+        form = None  # If no empty timeslots, set form to None
+    
     return render(request, 'rooms/room.html', {'room': room_data, 'timeslots':room_timeslots, 'form':form})
-
