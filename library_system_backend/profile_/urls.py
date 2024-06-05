8fdfb8467b13f50e2754943fd47c8502c22c3a87
ThienKianFoh <thienkianfoh@gmail.com>
2024-06-05 19:50:30 +0800

    Profile backend 100% completed

--- library_system_backend/profile_/urls.py
+++ library_system_backend/profile_/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('request-password-change/', views.request_password_change, name='request_password_change'),
+    path('update/', views.profile_update, name='profile_update'),
     path('<str:username>/', views.profile_, name='profile'),
     path('', views.profile_redirect, name='profile_redirect'),
 ]
